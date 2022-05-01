51b4385a847c6306c5401eb49ec6908e42cdb38c
Jeremy MacDougall <macdojer@outlook.com>
2022-05-01 00:23:04 -0400

    Handling HTTP verbs with custom logic

--- Pages/Admin/AddEditRecipe.cshtml.cs
+++ Pages/Admin/AddEditRecipe.cshtml.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 using TopsyTurvyCakes.Models;
 
 namespace TopsyTurvyCakes.Pages.Admin
 {
     public class AddEditRecipeModel : PageModel
     {
+        private readonly IRecipesService recipesService;
 
         [FromRoute]
         public long? Id
@@ -25,8 +27,29 @@ namespace TopsyTurvyCakes.Pages.Admin
         {
             get; set;
         }
-        public void OnGet()
+
+        public AddEditRecipeModel(IRecipesService recipesService)
         {
+            this.recipesService = recipesService;
+        }
+
+
+        public async Task OnGetAsync()  // one of the most powerful features // the ability to execute different code based on the http verb the request was made with
+        {
+            // load the data for the initial recipe
+
+            Recipe = await recipesService.FindAsync(Id.GetValueOrDefault()) ?? new Recipe(); // if it's not found, create a new recipe instead
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return RedirectToPage("/Recipe", new
+            {
+                id = Id
+            });
+            // check out razor pages documentation to see all the various actionResult types and usage
+
         }
     }
 }
